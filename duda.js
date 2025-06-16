3bbf945fb45c458db5fc74afbac040643a3630d6
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-06-16 11:20:19 -0300

    Add files via upload

--- duda.js
+++ duda.js
@@ -1,9 +1,9 @@
-/*
-Desenvolva um programa para calcular programa que efetue a leitura de dois valores numéricos inteiros. Processe a operação de adição dos dois valores e apresente na sequência a soma obtida com a operação.
-Fórmula: 
-calcular o F = C * 9/ 5 + 32, sendo F a temperatura em Fahenheit e C a temperatura em Celsius.
-*/
+valorA = prompt ("Digite  valor A")
+valorB = prompt("Digite valor B")
+valorC = ""
 
-temperatura = prompt("Digite uma temperatura em grau celsius")
- f = temperatura * 9/ 5 + 32
-alert(" Fahrenheit = Celsius * 9/5 + 32 graus é: " + f)
+valorC = valorA
+valorA = valorB
+valorB = valorC
+
+alert("A = " + valorA + "\n B = " + valorB)
