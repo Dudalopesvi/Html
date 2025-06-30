b379f99f2192dc5bd8434940e6eeebac7ccde66c
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-06-30 11:13:00 -0300

    do jeito do professor

--- ativadade_de_array.js
+++ ativadade_de_array.js
@@ -1,15 +1,16 @@
-const num = [3,7,18,21,33]
+const numeros = [3,7,18,21,33]
 
-function inserirFinalArray() {
-    var novonum = prompt("Digite um novo número:")
-    num.push(novonum)
-    console.log(num)
+function CalcularMedia() {
+ soma = 0, media = 0
+
+ for(i=0; i < numeros.length; i++) {
+    soma = soma + numeros[i];
+}
+ media = soma / numeros.length
+ alert (media)
+
 }
 
-function Calcularnum() {
-var novonum = prompt("Digite o número que você colocou antes: ")
-    media=(3+7+18+21+33+ novonum)/6
-    alert("A sua média é" + media)
-    console.log(num)
-}
+
+
 
