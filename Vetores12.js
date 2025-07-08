95581b71ad7e2aa5e82f277bd29b761cad3334be
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-07-08 11:22:27 -0300

    atividade do dia 08/07/2025

---
+++ Vetores12.js
@@ -0,0 +1,33 @@
+    const numeros = [3, 23, 45, -78, -90]
+
+
+ let conta = 0 
+ debugger
+let = menornum = numeros[1]
+
+console.log("Diga qual é o menor",menornum)
+
+    let contador = 0 
+ debugger
+let = maiornum = num[1]
+
+while (contador < num.lengt)  {
+ if (num[contador] > maiornum){
+maiornum = num[contador]
+}
+ if (num[conta] > menornum){}
+ maenornum = numeros[conta]
+contador++
+
+soma += numeros[0];
+i++;
+
+}
+console.log("Diga qual é o maior",maiornum)
+console.log("Diga qual é menor",menornum)
+
+
+
+
+
+
